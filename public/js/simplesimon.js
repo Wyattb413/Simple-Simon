c00f293436656d344c0ce1b89d10292efa67a811
Wyatt Barnes <wyattb413@gmail.com>
2016-08-18 10:31:54 -0500

    Added functional play button feature

--- public/js/simplesimon.js
+++ public/js/simplesimon.js
@@ -5,134 +5,141 @@ var randomSelection = "";
 var randomSelectionString = "";
 var userSelectionString = "";
 
-function randomNumberFunction(){
-	randomNumber = Math.floor((Math.random() * 7) + 1);
-	randomSelection = "hilt" + randomNumber + " ";
-	randomSelectionString += randomSelection;
-}
+$('#playButton').click(function(){
+
+	alert("Starting Game");
+
+	setTimeout(function() {
+
+		function randomNumberFunction(){
+			randomNumber = Math.floor((Math.random() * 7) + 1);
+			randomSelection = "hilt" + randomNumber + " ";
+			randomSelectionString += randomSelection;
+		}
 
 
-$('#hilt1').click(function(){
-	// console.log('#hilt1');
-	userSelectionString += "#hilt1" + " ";
-	console.log(userSelectionString);
-	$('#lightsaberRed').addClass('saberExtended');
-	setTimeout(function(){
-		$('#lightsaberRed').removeClass('saberExtended');
+		$('#hilt1').click(function(){
+			// console.log('#hilt1');
+			userSelectionString += "#hilt1" + " ";
+			console.log(userSelectionString);
+			$('#lightsaberRed').addClass('saberExtended');
+			setTimeout(function(){
+				$('#lightsaberRed').removeClass('saberExtended');
+			}, 650);
+		});
+
+		$('#hilt2').click(function(){
+			// console.log('#hilt2');
+			userSelectionString += "#hilt2" + " ";
+			console.log(userSelectionString);
+			$('#lightsaberPurple').addClass('saberExtended');
+			setTimeout(function(){
+				$('#lightsaberPurple').removeClass('saberExtended');
+			}, 650);
+		});
+
+		$('#hilt3').click(function(){
+			// console.log('#hilt3');
+			userSelectionString += "#hilt3" + " ";
+			console.log(userSelectionString);
+			$('#lightsaberBlue').addClass('saberExtended');
+			setTimeout(function(){
+				$('#lightsaberBlue').removeClass('saberExtended');
+			}, 650);
+		});
+
+		$('#hilt4').click(function(){
+			// console.log('#hilt4');
+			userSelectionString += "#hilt4" + " ";
+			console.log(userSelectionString);
+			$('#lightsaberGreen').addClass('saberExtended');
+			setTimeout(function(){
+				$('#lightsaberGreen').removeClass('saberExtended');
+			}, 650);
+		});
+
+		$('#hilt5').click(function(){
+			// console.log('#hilt5');
+			userSelectionString += "#hilt5" + " ";
+			console.log(userSelectionString);
+			$('#lightsaberWhite').addClass('saberExtended');
+			setTimeout(function(){
+				$('#lightsaberWhite').removeClass('saberExtended');
+			}, 650);
+		});
+
+		$('#hilt6').click(function(){
+			// console.log('#hilt6');
+			userSelectionString += "#hilt6" + " ";
+			console.log(userSelectionString);
+			$('#lightsaberOrange').addClass('saberExtended');
+			setTimeout(function(){
+				$('#lightsaberOrange').removeClass('saberExtended');
+			}, 650);
+		});
+
+		$('#hilt7').click(function(){
+			// console.log('#hilt7');
+			userSelectionString += "#hilt7" + " ";
+			console.log(userSelectionString);
+			$('#lightsaberYellow').addClass('saberExtended');
+			setTimeout(function(){
+				$('#lightsaberYellow').removeClass('saberExtended');
+			}, 650);
+		});
+
+
+		randomNumberFunction();
+		console.log(randomSelectionString);
+		console.log(userSelectionString);
+
+		switch (randomNumber) {
+			case 1:
+				$('#lightsaberRed').addClass('saberExtended');
+				setTimeout(function(){
+					$('#lightsaberRed').removeClass('saberExtended');
+				}, 650);
+				break;
+			case 2:
+				$('#lightsaberPurple').addClass('saberExtended');
+				setTimeout(function(){
+					$('#lightsaberPurple').removeClass('saberExtended');
+				}, 650);
+				break;
+			case 3:
+				$('#lightsaberBlue').addClass('saberExtended');
+				setTimeout(function(){
+					$('#lightsaberBlue').removeClass('saberExtended');
+				}, 650);
+				break;
+			case 4:
+				$('#lightsaberGreen').addClass('saberExtended');
+				setTimeout(function(){
+					$('#lightsaberGreen').removeClass('saberExtended');
+				}, 650);
+				break;
+			case 5:
+				$('#lightsaberWhite').addClass('saberExtended');
+				setTimeout(function(){
+					$('#lightsaberWhite').removeClass('saberExtended');
+				}, 650);
+				break;
+			case 6:
+				$('#lightsaberOrange').addClass('saberExtended');
+				setTimeout(function(){
+					$('#lightsaberOrange').removeClass('saberExtended');
+				}, 650);
+				break;
+			case 7:
+				$('#lightsaberYellow').addClass('saberExtended');
+				setTimeout(function(){
+					$('#lightsaberYellow').removeClass('saberExtended');
+				}, 650);
+				break;
+		};
 	}, 650);
+
 });
-
-$('#hilt2').click(function(){
-	// console.log('#hilt2');
-	userSelectionString += "#hilt2" + " ";
-	console.log(userSelectionString);
-	$('#lightsaberPurple').addClass('saberExtended');
-	setTimeout(function(){
-		$('#lightsaberPurple').removeClass('saberExtended');
-	}, 650);
-});
-
-$('#hilt3').click(function(){
-	// console.log('#hilt3');
-	userSelectionString += "#hilt3" + " ";
-	console.log(userSelectionString);
-	$('#lightsaberBlue').addClass('saberExtended');
-	setTimeout(function(){
-		$('#lightsaberBlue').removeClass('saberExtended');
-	}, 650);
-});
-
-$('#hilt4').click(function(){
-	// console.log('#hilt4');
-	userSelectionString += "#hilt4" + " ";
-	console.log(userSelectionString);
-	$('#lightsaberGreen').addClass('saberExtended');
-	setTimeout(function(){
-		$('#lightsaberGreen').removeClass('saberExtended');
-	}, 650);
-});
-
-$('#hilt5').click(function(){
-	// console.log('#hilt5');
-	userSelectionString += "#hilt5" + " ";
-	console.log(userSelectionString);
-	$('#lightsaberWhite').addClass('saberExtended');
-	setTimeout(function(){
-		$('#lightsaberWhite').removeClass('saberExtended');
-	}, 650);
-});
-
-$('#hilt6').click(function(){
-	// console.log('#hilt6');
-	userSelectionString += "#hilt6" + " ";
-	console.log(userSelectionString);
-	$('#lightsaberOrange').addClass('saberExtended');
-	setTimeout(function(){
-		$('#lightsaberOrange').removeClass('saberExtended');
-	}, 650);
-});
-
-$('#hilt7').click(function(){
-	// console.log('#hilt7');
-	userSelectionString += "#hilt7" + " ";
-	console.log(userSelectionString);
-	$('#lightsaberYellow').addClass('saberExtended');
-	setTimeout(function(){
-		$('#lightsaberYellow').removeClass('saberExtended');
-	}, 650);
-});
-
-
-randomNumberFunction();
-console.log(randomSelectionString);
-console.log(userSelectionString);
-
-switch (randomNumber) {
-	case 1:
-		$('#lightsaberRed').addClass('saberExtended');
-		setTimeout(function(){
-			$('#lightsaberRed').removeClass('saberExtended');
-		}, 650);
-		break;
-	case 2:
-		$('#lightsaberPurple').addClass('saberExtended');
-		setTimeout(function(){
-			$('#lightsaberPurple').removeClass('saberExtended');
-		}, 650);
-		break;
-	case 3:
-		$('#lightsaberBlue').addClass('saberExtended');
-		setTimeout(function(){
-			$('#lightsaberBlue').removeClass('saberExtended');
-		}, 650);
-		break;
-	case 4:
-		$('#lightsaberGreen').addClass('saberExtended');
-		setTimeout(function(){
-			$('#lightsaberGreen').removeClass('saberExtended');
-		}, 650);
-		break;
-	case 5:
-		$('#lightsaberWhite').addClass('saberExtended');
-		setTimeout(function(){
-			$('#lightsaberWhite').removeClass('saberExtended');
-		}, 650);
-		break;
-	case 6:
-		$('#lightsaberOrange').addClass('saberExtended');
-		setTimeout(function(){
-			$('#lightsaberOrange').removeClass('saberExtended');
-		}, 650);
-		break;
-	case 7:
-		$('#lightsaberYellow').addClass('saberExtended');
-		setTimeout(function(){
-			$('#lightsaberYellow').removeClass('saberExtended');
-		}, 650);
-		break;
-}
-
-
 
 
 
