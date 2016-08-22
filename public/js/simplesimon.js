d42eb7c80252eaa49e58e8df07dfd50a2a6b4d67
Wyatt Barnes <wyattb413@gmail.com>
2016-08-21 22:07:01 -0500

    Setting up random number generator

---
+++ public/js/simplesimon.js
@@ -0,0 +1,35 @@
+"use strict";
+// (function(){
+
+var randomNumber;
+
+
+function randomNumberFunction(){
+	randomNumber = Math.floor((Math.random() * 4) + 1);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// })();
