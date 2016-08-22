e2178394b7e1a0926f75aa48e4f63e652d2879f1
Wyatt Barnes <wyattb413@gmail.com>
2016-08-21 22:10:19 -0500

    added sequence array to keep track of randomly generated numbers

--- public/js/simplesimon.js
+++ public/js/simplesimon.js
@@ -2,10 +2,14 @@
 // (function(){
 
 var randomNumber;
+var sequence = [];
 
 
 function randomNumberFunction(){
 	randomNumber = Math.floor((Math.random() * 4) + 1);
+	sequence.push(randomNumber);
+	console.log(sequence);
+	console.log("sequence^");
 };
 
 
